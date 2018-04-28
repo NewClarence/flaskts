9e01eb2f5f8d652a929d2057017424002c5d6819
liuxinq <wxsx0526@126.com>
2018-04-28 14:06:51 +0800

    update form.py

--- app/form.py
+++ app/form.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
 
-from wtforms import StringField,SubmitField,PasswordField
-from wtforms.validators import Required,Email
+from wtforms import StringField,SubmitField,PasswordField,ValidationError
+from wtforms.validators import Required,Email,EqualTo
 from flask_wtf import FlaskForm
+from .models import Users
 
 #登录表单
 class Login_Form(FlaskForm):
@@ -14,6 +15,15 @@ class Login_Form(FlaskForm):
 #注册表单
 class Register_Form(FlaskForm):
     name=StringField('Name',validators=[Required()])
-    password=PasswordField('Password',validators=[Required()])
+    password=PasswordField('Password',validators=[Required(),EqualTo('password2', message='Passwords must match.')])
+    password2 = PasswordField('Confirm Password', validators=[Required()])
     email = StringField('Email', validators=[Required(), Email()])
     submit=SubmitField('register')
+
+    def validate_email(self, field):
+        if Users.query.filter_by(email=field.data).first():
+            raise ValidationError('Email already registered.')
+
+    def validate_name(self, field):
+        if Users.query.filter_by(name=field.data).first():
+            raise ValidationError('name already in use.')
